506a8d6b542a941abda7411f257a00637c36ca13
Abdulazeez Mubarak <abdulazeez-mubarak@torchlight.com>
2020-08-09 22:27:25 +0200

    Nodemon dev

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const hbs=require('hbs')
 
 
 const app=express() 
+const port = process.env.PORT || 3000
 
 //Define path for Express config 
 const publicDirectoryPath=path.join(__dirname,'../public') 
@@ -84,4 +85,4 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000)
+app.listen(port)
