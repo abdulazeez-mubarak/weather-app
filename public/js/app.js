bfacc8f33ab8eb5edd15f367f1e536f42959f85b
Abdulazeez Mubarak <abdulazeez-mubarak@torchlight.com>
2020-08-09 22:38:23 +0200

    localhost removed

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location= search.value 
     if (!location) {message2.textContent='Enter address'}
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
                 if (!data.error){
                     
